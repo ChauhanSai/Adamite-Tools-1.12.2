36f7cfa02fe5e3c4c7c12b5b22c9f87cd1d701d1
ChauhanSai <chauhansaimc@gmail.com>
2021-03-12 22:34:31 -0600

    3/12/21
    Added tinkers integration
    Added block recipe

--- src/main/java/ChauhanSaiMC/AdamiteTools/integrations/tconstruct/SmeltryRecipes.java
+++ src/main/java/ChauhanSaiMC/AdamiteTools/integrations/tconstruct/SmeltryRecipes.java
@@ -1,7 +1,26 @@
 package ChauhanSaiMC.AdamiteTools.integrations.tconstruct;
 
+import ChauhanSaiMC.AdamiteTools.init.ModBlocks;
+import ChauhanSaiMC.AdamiteTools.init.ModFluids;
+import ChauhanSaiMC.AdamiteTools.init.ModItems;
+import net.minecraft.item.ItemStack;
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+
 public class SmeltryRecipes {
     public static void init() {
+        TinkerRegistry.registerBasinCasting(new ItemStack(ModBlocks.adamiteBlock), ItemStack.EMPTY, ModFluids.moltenAdamite, 1296);
+        TinkerRegistry.registerTableCasting(new ItemStack(ModItems.adamite), TinkerSmeltery.castIngot, ModFluids.moltenAdamite, 144);
 
+        TinkerRegistry.registerMelting(ModItems.adamite,ModFluids.moltenAdamite,144);
+        TinkerRegistry.registerMelting(ModItems.adamiteAxe,ModFluids.moltenAdamite,144 * 3);
+        TinkerRegistry.registerMelting(ModItems.adamitePickaxe,ModFluids.moltenAdamite,144 * 3);
+        TinkerRegistry.registerMelting(ModItems.adamiteSpade,ModFluids.moltenAdamite,144);
+        TinkerRegistry.registerMelting(ModItems.adamiteSword,ModFluids.moltenAdamite,144 * 2);
+        TinkerRegistry.registerMelting(ModItems.adamiteHoe,ModFluids.moltenAdamite,144 * 2);
+        TinkerRegistry.registerMelting(ModItems.adamiteHelmet,ModFluids.moltenAdamite,144 * 5);
+        TinkerRegistry.registerMelting(ModItems.adamiteChestplate,ModFluids.moltenAdamite,144 * 8);
+        TinkerRegistry.registerMelting(ModItems.adamiteLeggings,ModFluids.moltenAdamite,144 * 7);
+        TinkerRegistry.registerMelting(ModItems.adamiteBoots,ModFluids.moltenAdamite,144 * 4);
     }
 }
--- src/main/java/ChauhanSaiMC/AdamiteTools/integrations/tconstruct/ToolMaterials.java
+++ src/main/java/ChauhanSaiMC/AdamiteTools/integrations/tconstruct/ToolMaterials.java
@@ -1,7 +1,19 @@
 package ChauhanSaiMC.AdamiteTools.integrations.tconstruct;
 
+import ChauhanSaiMC.AdamiteTools.AdamiteTools;
+import ChauhanSaiMC.AdamiteTools.init.ModFluids;
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.*;
+import slimeknights.tconstruct.library.utils.HarvestLevels;
+
 public class ToolMaterials {
     public static void preInit() {
-
+        Material adamite = new Material(AdamiteTools.MOD_ID+".adamite",0x09A544);
+        adamite.addItemIngot("ingotAdamite");
+        adamite.setFluid(ModFluids.moltenAdamite);
+        adamite.setCastable(true);
+        adamite.setCraftable(false);
+        TinkerRegistry.addMaterialStats(adamite, new HeadMaterialStats(3600, 8.25F, 6.03F, HarvestLevels.COBALT), new HandleMaterialStats(1.35F, 400), new ExtraMaterialStats(300), new BowMaterialStats(1.33F, 2.35F, 2.32F));
+        TinkerRegistry.integrate(adamite).preInit();
     }
 }
